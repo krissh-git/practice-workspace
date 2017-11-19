76a0b7c7b6a18718cf2fe1d2ae1dd01b5fec55ea
krishna <krissh.m@gmail.com>
2017-11-19 18:14:33 +0530

    country count

---
+++ hadoop/countrycount/src/com/example/hadoop/CountryCountMain.java
@@ -0,0 +1,74 @@
+package com.example.hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class CountryCountMain {
+
+
+    public static class SalesMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            final IntWritable out = new IntWritable(1);
+            String input = value.toString();
+            String[] salesSplit = input.split(",");
+            String countryName = salesSplit[7];
+            context.write(new Text(countryName), out);
+        }
+    }
+
+
+    public static class SalesReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable value : values) {
+                sum += value.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+
+        Job job = Job.getInstance(configuration);
+
+        job.setJobName("Country Count");
+        job.setJarByClass(CountryCountMain.class);
+        job.setMapperClass(SalesMapper.class);
+        job.setReducerClass(SalesReducer.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        Path path = new Path(args[1]);
+        path.getFileSystem(configuration).deleteOnExit(path);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+
+    }
+}
