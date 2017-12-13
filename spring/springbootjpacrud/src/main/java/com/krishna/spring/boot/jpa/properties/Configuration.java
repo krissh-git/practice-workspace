b65c1d9502ec2c419286eda49f00e9ef8ed62228
krishna <krissh.m@gmail.com>
2017-12-13 22:12:33 +0530

    spring boot-data-mongodb

--- spring/springbootjpacrud/src/main/java/com/krishna/spring/boot/jpa/properties/Configuration.java
+++ spring/springbootjpacrud/src/main/java/com/krishna/spring/boot/jpa/properties/Configuration.java
@@ -1,11 +1,13 @@
 package com.krishna.spring.boot.jpa.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component("config")
+@PropertySource("test.properties")
 @ConfigurationProperties(prefix = "app.data")
 public class Configuration {
 
