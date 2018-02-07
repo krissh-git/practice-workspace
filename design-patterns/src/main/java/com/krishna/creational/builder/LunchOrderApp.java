f4db30028ce3c4b8d8711723c339095cdeff5b63
krishna <krissh.m@gmail.com>
2018-02-08 00:24:26 +0530

    builder design patterns

---
+++ design-patterns/src/main/java/com/krishna/creational/builder/LunchOrderApp.java
@@ -0,0 +1,12 @@
+package com.krishna.creational.builder;
+
+public class LunchOrderApp {
+
+    public static void main(String[] args) {
+        LunchOrder.Builder builder = new LunchOrder.Builder();
+
+        LunchOrder lunchOrder = builder.bread("multi-grain").dressing("salad").meat("chicken").build();
+
+        System.out.println(lunchOrder);
+    }
+}
